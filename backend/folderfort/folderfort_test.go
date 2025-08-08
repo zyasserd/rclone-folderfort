db61bd3ded016a35d8e5a2235589115f6807a817
Zyad Yasser <25272036+zyasserd@users.noreply.github.com>
2025-08-08 09:53:28 +0400

    simplified the folderfort tests;

--- backend/folderfort/folderfort_test.go
+++ backend/folderfort/folderfort_test.go
@@ -1,10 +1,10 @@
 // Test FolderFort filesystem interface
-package folderfort
+package folderfort_test
 
 import (
 	"testing"
 
-	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/backend/folderfort"
 	"github.com/rclone/rclone/fstest"
 	"github.com/rclone/rclone/fstest/fstests"
 )
@@ -16,23 +16,6 @@ func TestIntegration(t *testing.T) {
 	}
 	fstests.Run(t, &fstests.Opt{
 		RemoteName: *fstest.RemoteName,
-		NilObject:  (*Object)(nil),
+		NilObject:  (*folderfort.Object)(nil),
 	})
-}
-
-// TestNew tests creation of new filesystem
-func TestNew(t *testing.T) {
-	if *fstest.RemoteName == "" {
-		t.Skip("Skipping as no remote name")
-	}
-	
-	// Create a temporary config
-	m := fstest.MakeRemoteName("folderfort", nil)
-	f, err := fs.NewFs(m)
-	if err != nil {
-		t.Fatalf("Failed to create new Fs: %v", err)
-	}
-	if f == nil {
-		t.Fatal("Expected non-nil Fs")
-	}
 }
